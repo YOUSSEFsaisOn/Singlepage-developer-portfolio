8a1157c71ef018236c7ceb39254248ab6e1c8efd
YOUSSEFsaisOn <1234yusid@gmail.com>
2024-08-15 01:22:45 +0300

    IMG
    all

---
+++ my projec/app.js
@@ -0,0 +1,12 @@
+function Mybuttun () {
+    return <button>i am a button </button>
+}
+
+export default function Myapp () {
+    return(
+    <div>
+        <h1>welcome to the course</h1>
+        <Mybuttun/>
+    </div>
+    )
+}
